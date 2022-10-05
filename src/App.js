d1ad1435199ddf00ca1797f8d86f1321c0bc04ba
dhthinh7 <dhthinh7@gmail.com>
2022-10-05 10:03:08 +0700

    Update left sidebar done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import MenuMain from './Components/MenuMain/MenuMain';
+import SidebarLeft from './Components/SidebarLeft/SidebarLeft';
+import JiraMainTemplate from './Templates/JiraMainTemplate';
 
 function App() {
   return (
     <div className="App">
-      jira
+      <Switch>
+        {/* <Route exact path="/" component={SidebarLeft}/> */}
+        {/* <Route exact path="/">
+          <SidebarLeft/>
+          <MenuMain/>
+          thinh
+        </Route> */}
+        
+        <JiraMainTemplate exact path="/"/>
+      </Switch>
+      
     </div>
   );
 }
